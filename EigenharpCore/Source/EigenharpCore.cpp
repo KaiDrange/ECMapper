6fec23cbba4137ebe085d2375aec80fbc823070c
KaiDrange <kaimdrange@hotmail.com>
2021-05-11 11:08:57 +0200

    Improve Core startup and shutdown

--- EigenharpCore/Source/EigenharpCore.cpp
+++ EigenharpCore/Source/EigenharpCore.cpp
@@ -1,49 +1,11 @@
 #include "EigenharpCore.h"
 
-static volatile int keepRunning = 1;
-
-void intHandler(int dummy) {
-    std::cerr  << "int handler called";
-    if(keepRunning==0) {
-        sleep(1);
-        exit(-1);
-    }
-    keepRunning = 0;
-}
-
-void* process(void* arg) {
-    EigenApi::Eigenharp *pE = static_cast<EigenApi::Eigenharp*>(arg);
-    while(keepRunning) {
-        pE->process();
-    }
-    return nullptr;
-}
-
-void makeThreadRealtime(std::thread& thread) {
-
-    int policy;
-    struct sched_param param;
-
-    pthread_getschedparam(thread.native_handle(), &policy, &param);
-    param.sched_priority = 95;
-    pthread_setschedparam(thread.native_handle(), SCHED_FIFO, &param);
-
-}
-
-const int senderPort = 7001;
-const int receiverPort = 7000;
-
-
-//==============================================================================
 EigenharpCore::EigenharpCore() : eigenApi("./") {
+    coreInstance = this;
 }
-
-const juce::String EigenharpCore:: getApplicationName() { return "EigenharpCore"; }
-const juce::String EigenharpCore::getApplicationVersion() { return "0.0.1"; }
-
 
 void EigenharpCore::initialise(const juce::String &) {
-    signal(SIGINT, intHandler);
+    signal(SIGINT, EigenharpCore::intHandler);
     osc.connectSender("127.0.0.1", senderPort);
     osc.connectReceiver(receiverPort);
 
@@ -53,15 +15,43 @@ void EigenharpCore::initialise(const juce::String &) {
         std::cout  << "unable to start EigenLite";
     }
 
-    eigenApiProcessThread = std::thread(process, &eigenApi);
+    eigenApiProcessThread = std::thread(coreInstance->process, &eigenApi);
 }
+
 void EigenharpCore::shutdown() {
     std::cout << "Trying to quit gracefully";
+    if(keepRunning==0) {
+        sleep(1);
+        exit(-1);
+    }
     keepRunning = 0;
-    sleep(1);
     eigenApiProcessThread.join();
     eigenApi.stop();
     osc.disconnectReceiver();
     osc.disconnectSender();
+}
+
+void EigenharpCore::intHandler(int dummy) {
+    std::cerr  << "int handler called";
+    coreInstance->systemRequestedQuit();
+}
+
+void* EigenharpCore::process(void* arg) {
+    EigenApi::Eigenharp *pE = static_cast<EigenApi::Eigenharp*>(arg);
+    while(keepRunning) {
+        pE->process();
+    }
+    return nullptr;
+}
+
+void EigenharpCore::makeThreadRealtime(std::thread& thread) {
+
+    int policy;
+    struct sched_param param;
+
+    pthread_getschedparam(thread.native_handle(), &policy, &param);
+    param.sched_priority = 95;
+    pthread_setschedparam(thread.native_handle(), SCHED_FIFO, &param);
+
 }
 
--- EigenharpCore/Source/EigenharpCore.h
+++ EigenharpCore/Source/EigenharpCore.h
@@ -9,14 +9,25 @@
 class EigenharpCore : public juce::JUCEApplication {
 public:
     EigenharpCore();
-    const juce::String getApplicationName() override;
-    const juce::String getApplicationVersion() override;
+    const juce::String getApplicationName() override { return "EigenharpCore"; }
+    const juce::String getApplicationVersion() override { return "0.0.1"; }
     void initialise(const juce::String &) override;
     void shutdown() override;
+
 private:
     EigenApi::Eigenharp eigenApi;
     OSCCommunication osc;
     std::thread eigenApiProcessThread;
+    static void intHandler(int dummy);
+    static void* process(void* arg);
+    void makeThreadRealtime(std::thread& thread);
+    
+    const int senderPort = 7001;
+    const int receiverPort = 7000;
+
 };
+
+static EigenharpCore *coreInstance;
+static volatile int keepRunning = 1;
 
 START_JUCE_APPLICATION (EigenharpCore)
