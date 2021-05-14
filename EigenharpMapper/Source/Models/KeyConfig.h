e44b5e188c68231e5e20119527f1d39b2409dd60
KaiDrange <kaimdrange@hotmail.com>
2021-05-14 15:07:52 +0200

    Rename MappedKey to KeyConfig

---
+++ EigenharpMapper/Source/Models/KeyConfig.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <JuceHeader.h>
+#include "Enums.h"
+
+class KeyConfig {
+public:
+    KeyConfig(EigenharpKeyType keyType, juce::ValueTree &rootValueTree);
+    KeyConfig(juce::ValueTree &keyTree);
+    
+    struct KeyId {
+        int course = 0;
+        int keyNo = 0;
+        bool equals(KeyId &id) { return course == id.course && keyNo == id.keyNo; }
+    };
+    
+    KeyId getKeyId() const;
+    void setKeyId(KeyId keyId);
+    EigenharpKeyType getKeyType() const;
+    void setKeyType(EigenharpKeyType keyType);
+    KeyColour getKeyColour() const;
+    void setKeyColour(KeyColour colour);
+    Zone getZone() const;
+    void setZone(Zone zone);
+    KeyMappingType getMappingType() const;
+    void setMappingType(KeyMappingType mappingType);
+    juce::String getMappingValue() const;
+    void setMappingValue(juce::String mappingValue);
+    juce::ValueTree getValueTree() const;
+    void setValueTree(juce::ValueTree valueTree);
+
+private:
+    juce::Identifier id_keyNo = "keyNo";
+    juce::Identifier id_course = "course";
+    juce::Identifier id_keyType = "keyType";
+    juce::Identifier id_keyColour = "keyColour";
+    juce::Identifier id_keyMappingType = "keyMappingType";
+    juce::Identifier id_mappingValue = "mappingValue";
+    juce::Identifier id_zone = "zone";
+    juce::ValueTree valueTree;
+    
+};
