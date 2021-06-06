a978449e8a450ae087a16dc3063b98671136df29
KaiDrange <kaimdrange@hotmail.com>
2021-06-06 22:30:41 +0200

    Rename EigenharpCore to EigenCore

---
+++ EigenCore/JuceLibraryCode/BinaryData.h
@@ -0,0 +1,45 @@
+/* =========================================================================================
+
+   This is an auto-generated file: Any edits you make may be overwritten!
+
+*/
+
+#pragma once
+
+namespace BinaryData
+{
+    extern const char*   _69eigenharp_rules;
+    const int            _69eigenharp_rulesSize = 767;
+
+    extern const char*   bs_mm_fw_0103_ihx;
+    const int            bs_mm_fw_0103_ihxSize = 20326;
+
+    extern const char*   libeigenapi_dylib;
+    const int            libeigenapi_dylibSize = 331344;
+
+    extern const char*   libpicodecoder_dylib;
+    const int            libpicodecoder_dylibSize = 60216;
+
+    extern const char*   pico_ihx;
+    const int            pico_ihxSize = 24114;
+
+    extern const char*   psu_mm_fw_0102_ihx;
+    const int            psu_mm_fw_0102_ihxSize = 17752;
+
+    // Number of elements in the namedResourceList and originalFileNames arrays.
+    const int namedResourceListSize = 6;
+
+    // Points to the start of a list of resource names.
+    extern const char* namedResourceList[];
+
+    // Points to the start of a list of resource filenames.
+    extern const char* originalFilenames[];
+
+    // If you provide the name of one of the binary resource variables above, this function will
+    // return the corresponding data and its size (or a null pointer if the name isn't found).
+    const char* getNamedResource (const char* resourceNameUTF8, int& dataSizeInBytes);
+
+    // If you provide the name of one of the binary resource variables above, this function will
+    // return the corresponding original, non-mangled filename (or a null pointer if the name isn't found).
+    const char* getNamedResourceOriginalFilename (const char* resourceNameUTF8);
+}
