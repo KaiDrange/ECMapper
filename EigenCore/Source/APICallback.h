43b4135af335cf988dda6d0964db6bff046ca28d
KaiDrange <kaimdrange@hotmail.com>
2021-06-06 22:34:34 +0200

    Rename EigenharpCore to EigenCore

---
+++ EigenCore/Source/APICallback.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <JuceHeader.h>
+#include <unistd.h>
+#include <signal.h>
+#include <eigenapi.h>
+#include <iostream>
+#include <thread>
+#include "OSCCommunication.h"
+#include "Enums.h"
+
+struct ConnectedDevice {
+    const char *dev;
+    EHDeviceType type = EHDeviceType::None;
+    int assignedLEDColours[3][120]; // course, key
+    bool activeKeys[3][120]; // course, key
+};
+extern std::list<ConnectedDevice> connectedDevices;
+
+
+class APICallback: public EigenApi::Callback {
+public:
+    APICallback(EigenApi::Eigenharp& eh, OSC::OSCMessageFifo *sendQueue);
+    virtual void device(const char* dev, DeviceType dt, int rows, int cols, int ribbons, int pedals);
+    virtual void key(const char* dev, unsigned long long t, unsigned course, unsigned key, bool a, unsigned p, int r, int y);
+    virtual void breath(const char* dev, unsigned long long t, unsigned val);
+    virtual void strip(const char* dev, unsigned long long t, unsigned strip, unsigned val);
+    virtual void pedal(const char* dev, unsigned long long t, unsigned pedal, unsigned val);
+    
+    EigenApi::Eigenharp& eh_;
+
+private:
+    OSC::OSCMessageFifo *sendQueue;
+    EHDeviceType getTypeFromDev(const char* dev) const;
+};
