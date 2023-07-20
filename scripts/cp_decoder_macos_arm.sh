329cd2626922f9e35abfec9a70b9600693bcc66b
TheTechnobear <mharris@technobear.com>
2023-07-20 12:54:29 +0200

    fix rpath , kind of

---
+++ scripts/cp_decoder_macos_arm.sh
@@ -0,0 +1,3 @@
+#/bin/sh
+mkdir  ~/Library/Audio/Plug-Ins/VST3/ECCO.vst3/Contents/Frameworks
+cp ./EigenLite/resources/libpico_decoder_arm64_1_0_0.dylib ~/Library/Audio/Plug-Ins/VST3/ECCO.vst3/Contents/Frameworks/libpicodecoder.dylib
